b03612bfebc024f6ee5d8cb183fd9b94f33a96fd
CPoojaPriyadarshini <poojachandrabanu@gmail.com>
2017-02-12 14:14:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/intro', function(req, res){
+    res.send("Pooja from BSAU");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
