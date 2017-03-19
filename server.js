9d52de4ec934d9f0614ffdf3dfa5a853680e72b3
CPoojaPriyadarshini <poojachandrabanu@gmail.com>
2017-03-19 12:19:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('https://raw.githubusercontent.com/CPoojaPriyadarshini/imad-app-v2/master/DSC_0448.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'DSC_0448'));
 });
 
 app.get('/ui/main.js', function (req, res) {
